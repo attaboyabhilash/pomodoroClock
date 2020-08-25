ed9818d13f1b957cae067a37d4ab4d01e9ab5b6f
Abhilash Negi <tiger.abhi.3609@gmail.com>
2020-08-25 21:00:43 +0530

    Updates

---
+++ precache-manifest.fc06cc3f54fee19ffaffa42c16725ab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c97232cf5586520cddd6fa05d5643b4",
+    "url": "/pomodoroClock/index.html"
+  },
+  {
+    "revision": "5b4fa317364ad3f21b2c",
+    "url": "/pomodoroClock/static/css/main.30923d52.chunk.css"
+  },
+  {
+    "revision": "3594308e160721652a52",
+    "url": "/pomodoroClock/static/js/2.0ae2f92b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoroClock/static/js/2.0ae2f92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4fa317364ad3f21b2c",
+    "url": "/pomodoroClock/static/js/main.48603cad.chunk.js"
+  },
+  {
+    "revision": "01ad8c670281af1c7683",
+    "url": "/pomodoroClock/static/js/runtime-main.69a22b20.js"
+  },
+  {
+    "revision": "a4e328cfaca98b713cba8ea431b9f7e0",
+    "url": "/pomodoroClock/static/media/myMainLogo.a4e328cf.svg"
+  }
+]);
